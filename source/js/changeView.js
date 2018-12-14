c943ebd7422b79695138ae81e360a8f73e665731
webistomin <webistomin@gmail.com>
2018-12-14 20:43:29 +0300

    Правки дизайна

--- source/js/changeView.js
+++ source/js/changeView.js
@@ -3,11 +3,19 @@ const buttonGrid = document.getElementById('gridView');
 const moviesList = document.querySelector('.movies__list');
 
 buttonRow.addEventListener('click', () => {
+  if (!buttonRow.classList.contains('page-header__button-active')) {
+    buttonRow.classList.add('page-header__button--active');
+    buttonGrid.classList.remove('page-header__button--active');
+  }
   moviesList.classList.remove('movies__list--grid');
   moviesList.classList.add('movies__list--row');
 });
 
 buttonGrid.addEventListener('click', () => {
+  if (!buttonGrid.classList.contains('page-header__button-active')) {
+    buttonGrid.classList.add('page-header__button--active');
+    buttonRow.classList.remove('page-header__button--active');
+  }
   moviesList.classList.add('movies__list--grid');
   moviesList.classList.remove('movies__list--row');
 });

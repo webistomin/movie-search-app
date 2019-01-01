ebc82fee551ea77147548d029c446026fb7dee31
webistomin <webistomin@gmail.com>
2019-01-01 15:11:19 +0300

    Правки

---
+++ src/components/NavigationPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 class="movies__title">{{$route.meta.title}}</h2>
+    <MoviesList></MoviesList>
+  </div>
+</template>
+
+<script>
+  import MoviesList from './MoviesList';
+
+  export default {
+    name: 'NavigationPage',
+    components: { MoviesList },
+    mounted() {
+      console.log(this.$route);
+    },
+  };
+</script>
+
+<style>
+
+</style>
--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="page-content">
     <Intro/>
-    <section class="movies">
+    <section class="movies" :class="{'movies--noscroll' : isNoScroll}">
       <preloader v-if="1 === 0"/>
-      <MoviesList v-else/>
+      <transition name="fade" mode="out-in" v-else>
+        <router-view :key="$route.fullPath"/>
+      </transition>
     </section>
     <Socials/>
   </main>
@@ -19,11 +21,54 @@
   export default {
     name: 'PageContent',
     components: { Preloader, MoviesPage, Socials, MoviesList, Intro },
+    computed: {
+      isNoScroll() {
+        return this.$route.meta.noScroll;
+      },
+    },
   };
 </script>
 
 <style lang="sass">
+  .fade-enter
+    opacity: 0
+
+  .fade-enter-active
+    transition: opacity 0.3s ease
+
+  .fade-leave-active
+    transition: opacity 0.3s ease
+    opacity: 0
+
   .page-content
     @media (min-width: $screen-md)
       display: flex
+
+    .movies
+      position: relative
+      box-sizing: border-box
+      background-color: $color-brown--secondary
+
+      @media (min-width: $screen-md)
+        width: 60vw
+        flex-shrink: 0
+        overflow-y: scroll
+        height: calc(100vh - 60px)
+
+        &--noscroll
+          overflow: hidden
+
+        &::-webkit-scrollbar-track
+          border-radius: 10px
+          background-color: transparent
+          background-clip: content-box
+
+        &::-webkit-scrollbar
+          width: 7px
+          background-color: transparent
+
+        &::-webkit-scrollbar-thumb
+          border-radius: 10px
+          -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3)
+          background-color: $color-gold
 </style>

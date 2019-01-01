acce3ebee6e158a2c4caa50fd8b2dcf48ef0a124
Alexey Istomin <webistomin@gmail.com>
2019-01-01 14:49:33 +0300

    Update PageContent.vue

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -26,4 +26,32 @@
   .page-content
     @media (min-width: $screen-md)
       display: flex
+      
+  .movies
+    position: relative
+    box-sizing: border-box
+    background-color: $color-brown--secondary
+
+    @media (min-width: $screen-md)
+      width: 60vw
+      flex-shrink: 0
+      overflow-y: scroll
+      height: calc(100vh - 60px)
+
+      &--noscroll
+        overflow: hidden
+
+      &::-webkit-scrollbar-track
+        border-radius: 10px
+        background-color: transparent
+        background-clip: content-box
+
+      &::-webkit-scrollbar
+        width: 7px
+        background-color: transparent
+
+      &::-webkit-scrollbar-thumb
+        border-radius: 10px
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, .3)
+        background-color: $color-gold   
 </style>

d9fedbe38d758af412d3eb979af0573e326e04c9
webistomin <webistomin@gmail.com>
2019-01-01 16:34:46 +0300

    Правки

--- src/components/BestList.vue
+++ src/components/BestList.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="best__list">
-    <best-item
+    <best-list-item
       v-for="(item, index) of moviesList.slice(0,5)"
       :movie-title="item.title.slice(0,10)"
       :movie-poster="item.poster_path"
